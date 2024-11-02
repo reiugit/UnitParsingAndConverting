7fb9e14147d2d923b2dbae55bdd13e7372362c2e
reiugit <67372053+reiugit@users.noreply.github.com>
2024-11-02 12:24:45 +0100

    Projektdateien hinzufügen.

---
+++ UnitParsingAndConverting/Program.cs
@@ -0,0 +1,32 @@
+﻿using UnitsNet;
+using UnitsNet.Units;
+using static System.Console;
+
+// Speed Examples
+WriteLine("==== Speed Examples ============");
+
+var speed = Speed.FromKilometersPerHour(100);
+WriteLine($"\n     Speed: {speed}");
+
+speed = speed.ToUnit(SpeedUnit.MilePerHour);
+WriteLine($"\n     Speed: {speed}");
+
+WriteLine($"     Value: {speed.Value}");
+WriteLine($"     Unit:  {speed.Unit}");
+
+WriteLine($"\n     MetersPerSecond: {speed.MetersPerSecond}");
+
+// Temperature Examples
+WriteLine("\n==== Temperature Examples ====");
+
+var tempString = "20 °C";
+var temperature = Temperature.Parse(tempString);
+WriteLine($"\n     Temperature: {temperature}");
+
+temperature = temperature.ToUnit(TemperatureUnit.DegreeFahrenheit);
+WriteLine($"     Temperature: {temperature}");
+
+WriteLine($"\n     DegreesDelisle: {temperature.DegreesDelisle}");
+WriteLine($"     Unit: {TemperatureUnit.DegreeDelisle}");
+
+WriteLine("\n===============================");
